1905ec9a755eb545a41a0da9e542f9f1a2653294
Shlomate <shlomo@promate.co.il>
2025-07-28 20:32:49 +0300

    Create OneSignalSDKWorker.js

---
+++ public/OneSignalSDKWorker.js
@@ -0,0 +1,9 @@
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+  <script>
+    window.OneSignalDeferred = window.OneSignalDeferred || [];
+    OneSignalDeferred.push(async function(OneSignal) {
+      await OneSignal.init({
+        appId: "481e17b3-02d0-4b2c-9b8b-18f1b606e8d2",
+      });
+    });
+  </script>
